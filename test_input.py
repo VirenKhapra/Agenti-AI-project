1ac9be2eba448f5f05430660e9e95563707fc149
VirenKhapra <virenkhapra123@gmail.com>
2026-05-13 10:01:46 +0530

    test_input.py

--- test_input.py
+++ test_input.py
@@ -1,14 +1,22 @@
 from data_input import get_email_text
 from llm_extractor import extract_data
+from validator_agent import validate_extracted_data
 
-print("🚀 Running full pipeline...")
-
+# 🔹 STEP 1  Get Email
 email_text = get_email_text()
 
-print("\n📩 RAW EMAIL:\n")
+print("\n📩 ORIGINAL EMAIL:\n")
 print(email_text)
 
-result = extract_data(email_text)
+# 🔹 STEP 2  Extract Data using LLM
+extracted_json = extract_data(email_text)
 
+# 🔹 STEP 3  Validate Extracted Data
+verified_json = validate_extracted_data(
+    email_text,
+    extracted_json
+)
+
+# 🔹 STEP 4 → Final Output
 print("\n✅ FINAL OUTPUT:\n")
-print(result)
+print(verified_json)
